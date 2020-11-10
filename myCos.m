761b6d89326848ccd2f0120b667f8e84eb1062f5
p3t3rsz <xszedlak@stuba.sk>
2020-11-10 16:25:31 +0100

    myCosA

---
+++ myCos.m
@@ -0,0 +1,28 @@
+%% myCos
+
+%% Function domain
+x = [0:0.1:2*pi]';
+
+%% Function values
+y_cos = cos(x);
+
+%% Figure initialization
+figure(1), hold on, box on
+
+%% Plotting figure
+plot(x, y_cos, 'm-.')
+
+%% Grid
+grid on
+
+%% x-label
+xlabel('x_A')
+
+%% y-label
+ylabel('y_A')
+
+%% Title
+title('y_A = cos(x_A) ')
+
+%% Legend
+legend(' y_A ')
